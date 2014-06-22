3c62be43175ecca8773810848be17ce74e253c50
yannick <yannick.dominguez@gmail.com>
2014-06-22 20:06:16 +0200

    externalised survey retrieval

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var expressHbs = require('express3-handlebars');
-var shortId = require('shortid');
 
 //set up mongo connection
 var mongo = require('mongodb');
@@ -11,6 +10,7 @@ var monk = require('monk');
 var db = monk('localhost:27017/tuveux');
 
 var getResults = require('./lib/results');
+var survey = require('./lib/survey')(db);
 
 var server = express();
 
@@ -28,34 +28,22 @@ server.use(bodyParser.urlencoded());
 //return survey at the provided key
 server.get('/:key', function (req, res) {
 
-    var surveys = db.get('surveys');
-    surveys.find({key: req.params.key}, {}, function (err, docs) {
-        if (err) return res.send('Could not read from db');
-
+    survey.get(req.params.key, function (err, survey) {
         //redirect to index if key doesn't exist
-        if(docs.length === 0 ) return res.redirect('/');
+        if (err) return res.redirect('/');
 
-        docs[0].poll = getResults(docs[0].answers);
-
-        res.render('survey.hbs', docs[0]);
+        //add poll results
+        survey.poll = getResults(survey.answers);
+        res.render('survey.hbs', survey);
     });
 });
 
 //create a new survey
 server.post('/survey', function (req, res) {
 
-    //pseudo unique key for survey
-    var key = shortId.generate();
-
-    var surveys = db.get('surveys');
-    surveys.insert({
-        key: key,
-        survey: req.body.survey,
-        answers: []
-    },
-    function (err, doc) {
-        if (err) return res.send('There was a problem writing to the database');
-        res.redirect('/' + key);
+    survey.create(req.body.survey, function (err, survey) {
+        if (err) res.send(err);
+        res.redirect('/' + survey.key);
     });
 });
 

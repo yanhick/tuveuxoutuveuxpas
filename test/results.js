28051bf24e92e4b4a61efce2d180902b1f28491b
yannick <yannick.dominguez@gmail.com>
2014-06-22 20:55:51 +0200

    adding tests

--- test/results.js
+++ test/results.js
@@ -1,6 +1,49 @@
+'use strict';
+
+var sinon = require('sinon');
+var expect = require('chai').expect;
+
+var getResults = require('../lib/results');
 
 describe('poll result', function () {
-    it('should return a positive poll');
-    it('should return a negative poll');
-    it('should return an equal poll');
+    it('should return a positive poll', function () {
+        var results = getResults([{
+            name: 'me',
+            answer: true
+        }]);
+
+        expect(results).to.eql({
+            accepted: true,
+            refused: false,
+            equals: false
+        });
+    });
+    it('should return a negative poll', function () {
+        var results = getResults([{
+            name: 'me',
+            answer: false
+        }]);
+
+        expect(results).to.eql({
+            accepted: false,
+            refused: true,
+            equals: false
+        });
+
+    });
+    it('should return an equal poll', function () {
+        var results = getResults([{
+            name: 'me',
+            answer: false
+        }, {
+            name: 'you',
+            answer: true
+        }]);
+
+        expect(results).to.eql({
+            accepted: false,
+            refused: false,
+            equals: true
+        });
+    });
 });
--- test/survey.js
+++ test/survey.js
@@ -1,17 +1,91 @@
+'use strict';
+
+var sinon = require('sinon');
+var chai = require('chai');
+var sinonChai = require('sinon-chai');
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var shortId = require('shortid');
+
+var surveyFactory = require('../lib/survey');
 
 describe('survey', function () {
+    var survey, findStub, getStub, insertStub, updateStub;
+
+    beforeEach(function () {
+        findStub = sinon.stub();
+        getStub = sinon.stub();
+
+        insertStub = sinon.stub();
+        insertStub.callsArg(1);
+
+        updateStub = sinon.stub();
+        updateStub.callsArg(1);
+
+        getStub.returns({
+            find: findStub,
+            insert: insertStub,
+            update: updateStub
+        });
+
+        survey = surveyFactory({
+            get: getStub
+        });
+
+        var shortIdStub = sinon.stub(shortId, 'generate');
+        shortIdStub.returns('abcd');
+    });
+
+    afterEach(function () {
+        shortId.generate.restore();
+    });
+
     describe('#create', function () {
-        it('should create a new survey');
-        it('should throw when can\'t create');
+
+        it('should create a new survey', function (done) {
+            survey.create('my question', function (err, doc) {
+                if (err) done(err);
+
+                expect(insertStub).to.have.been.calledWith({
+                    key: 'abcd',
+                    survey: 'my question',
+                    answers: []
+                });
+                done();
+            });
+        });
     });
 
     describe('#addAnswer', function () {
-        it('should add an answer to the survey');
-        it('should throw when can\t update');
+        var surveyGetStub;
+
+        beforeEach(function () {
+            surveyGetStub = sinon.stub(survey, 'get');
+            surveyGetStub.callsArgWith(1, null, {
+                survey: 'my question',
+                key: 'abcd',
+                answers: []
+            });
+        });
+
+        afterEach(function () {
+            survey.get.restore();
+        });
+
+        it('should add an answer to the survey', function (done) {
+            survey.addAnswer('abcd', {name: 'me', answer: true}, function (err, done) {
+                if (err) done(err);
+
+                expect(getStub).to.have.been.calledWith('abcd', sinon.match.func);
+                done();
+            });
+        });
     });
 
     describe('#get', function () {
-        it('should fetch an existing survey');
-        it('should throw for a non-existing survey');
+        it('should fetch an existing survey', function () {
+
+        });
     });
 });
